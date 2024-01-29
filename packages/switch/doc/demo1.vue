3c274f6e4c618fe4e56ad9d98bcded3f89f60e24
Sunny020720 <1538052461@qq.com>
2024-01-30 00:11:25 +0800

    fix:switch的demo1样式修改

--- packages/switch/doc/demo1.vue
+++ packages/switch/doc/demo1.vue
@@ -1,9 +1,11 @@
 <template>
-  <l-switch :value="value1" @update:value="updateSwitchValue" />
-  <l-switch
-      :value="value1" @update:value="updateSwitchValue"
-      activeColor="#13ce66" inactiveColor="#ff4949"
-  />
+  <div class="l-row">
+    <l-switch :value="value1" @update:value="updateSwitchValue" />
+    <l-switch
+        :value="value1" @update:value="updateSwitchValue"
+        activeColor="#13ce66" inactiveColor="#ff4949"
+    />
+  </div>
   <p>开关状态: {{value1}}</p>
 </template>
 
@@ -16,3 +18,15 @@ const updateSwitchValue=(newValue:boolean)=>{
   value1.value=newValue
 }
 </script>
+
+<style scoped lang="scss">
+.l-row {
+  display: flex;
+  .l-switch + .l-switch {
+    margin-left: 12px;
+  }
+}
+.l-row + .l-row {
+  margin-top: 10px;
+}
+</style>
