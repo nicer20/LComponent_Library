f19cae71a55489c139f9afc6ba42290b6d9a0bc9
StarDust-Zou <70910029+StarDust-Zou@users.noreply.github.com>
2024-02-16 14:57:48 +0800

    feat: 创建文件夹

---
+++ packages/layout/doc/Demo.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Layout 布局</h1>
+  <p>通过基础的 24 分栏，迅速简便地创建布局。</p>
+  <h2>基础布局</h2>
+  <p>使用列创建基础网格布局。</p>
+  通过 <code>row</code> 和 <code>col</code> 组件，并通过 <code>col</code> 组件的 <code>span</code> 属性我们就可以自由地组合布局。
+</template>
+<script lang="ts" setup>
+import Demo1 from '../doc/demo1.vue'
+</script>
+<style scoped lang="scss">
+@import '../../style/exampleShowcase.scss';
+</style>
